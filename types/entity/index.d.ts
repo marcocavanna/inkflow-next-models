deb920ca207a3a41b56e1528e293eac683b8ce71
Marco Cavanna <marco@appbuckets.io>
2021-01-21 18:19:03 +0100

    feat(entity): base production order entity

--- types/entity/index.d.ts
+++ types/entity/index.d.ts
@@ -1,5 +1,7 @@
 export { ContactEntity } from './Contact';
 
+export { ProductionOrderEntity } from './ProductionOrder';
+
 export { RegistryEntity } from './Registry';
 
 export { UserEntity } from './User';
